ed47c2a4a486846dc7a1728e34756b9891e9d956
Ali-thepro <aliahmad2005yt@gmail.com>
2024-08-12 14:44:37 +0100

    add pages

--- blogapp-frontend/src/App.jsx
+++ blogapp-frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">Blog App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   )
 }
