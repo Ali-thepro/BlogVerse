439a5a428c640d127124151a8a7f95d31f2e242c
Ali-thepro <aliahmad2005yt@gmail.com>
2024-08-12 17:55:58 +0100

    connect to mongoDB

--- blogapp-backend/app.js
+++ blogapp-backend/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
+const middleware = require('./utils/middleware');
 
 mongoose.set('strictQuery', false)
 
@@ -19,10 +20,14 @@ mongoose.connect(config.MONGODB_URI)
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
-
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.get('/', (request, response) => { 
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.use(middleware.unknownEndpoint)
 
 module.exports = app;
